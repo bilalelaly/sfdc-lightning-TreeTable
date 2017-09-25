c25bc8ed3a6b682b7fbe0cf22ebc18d9ed717640
bilalelaly <32270257+bilalelaly@users.noreply.github.com>
2017-09-25 17:14:21 +0200

    Create NewTreeNodeComponentTestHelper.js

---
+++ src/aura/NewTreeNodeComponentTestHelper.js
@@ -0,0 +1,12 @@
+({
+    // Fetch the roles from the Apex controller
+    getRoles: function(component) {
+        var action = component.get('c.getAllRoles');
+        // Set up the callback
+        var self = this;
+        action.setCallback(this, function(actionResult) {
+            component.set('v.rolesWrappers', actionResult.getReturnValue());
+        });
+        $A.enqueueAction(action);
+    },
+})
